17e2c9471896f0799fec99a997c5a7fd77d2ed81
David <vksdc@hotmail.com>
2023-02-19 17:12:31 +0100

    Service implementation

--- src/main/java/rodriguez/garcia/vanessa/calculator/service/SimpleOperationsApiServiceImpl.java
+++ src/main/java/rodriguez/garcia/vanessa/calculator/service/SimpleOperationsApiServiceImpl.java
@@ -4,20 +4,30 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 @Component
 public class SimpleOperationsApiServiceImpl implements SimpleOperationsApiService {
 
 	@Override
 	public BigDecimal add(List<BigDecimal> operators) {
-		// TODO Auto-generated method stub
-		return null;
+		if (CollectionUtils.isEmpty(operators) || operators.size() == 1) {
+			throw new IllegalArgumentException("The minimum number of operators required to perform the operation is 2");
+		}
+		
+		final BigDecimal result = operators.stream().reduce(BigDecimal::add).get();
+		
+		return result;
 	}
 
 	@Override
 	public BigDecimal diff(List<BigDecimal> operators) {
-		// TODO Auto-generated method stub
-		return null;
+		if (CollectionUtils.isEmpty(operators) || operators.size() == 1) {
+			throw new IllegalArgumentException("The minimum number of operators required to perform the operation is 2");
+		}
+		
+		final BigDecimal result = operators.stream().reduce((num1, num2) -> num1.subtract(num2)).get();
+		
+		return result;
 	}
-
 }
--- src/test/java/rodriguez/garcia/vanessa/calculator/service/SimpleOperationsApiServiceTest.java
+++ src/test/java/rodriguez/garcia/vanessa/calculator/service/SimpleOperationsApiServiceTest.java
@@ -9,12 +9,14 @@ import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class SimpleOperationsApiServiceTest {
 
-	private SimpleOperationsApiService service;
+	@Spy
+	private SimpleOperationsApiServiceImpl service;
 	
 	@Test
 	void addSuccessfullyTest() {
@@ -54,7 +56,7 @@ class SimpleOperationsApiServiceTest {
 		final BigDecimal operator1 = new BigDecimal(1);
 		final BigDecimal operator2 = new BigDecimal(2);
 		final List<BigDecimal> operators = List.of(operator1, operator2);
-		final BigDecimal expectedResult = new BigDecimal(3);
+		final BigDecimal expectedResult = new BigDecimal(-1);
 		
 		final BigDecimal result = service.diff(operators);
 
