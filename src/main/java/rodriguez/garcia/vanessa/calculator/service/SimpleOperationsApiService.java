f3b36a6a3df6bc401e8f41ca4f3040415499fd1a
David <vksdc@hotmail.com>
2023-02-19 17:33:27 +0100

    javadoc

--- src/main/java/rodriguez/garcia/vanessa/calculator/service/SimpleOperationsApiService.java
+++ src/main/java/rodriguez/garcia/vanessa/calculator/service/SimpleOperationsApiService.java
@@ -5,7 +5,23 @@ import java.util.List;
 
 public interface SimpleOperationsApiService {
 
+	/**
+	 * Method that returns the sum of the given list, said list must contain two
+	 * operators at least, an {@link IllegalArgumentException} will be thrown
+	 * otherwise.
+	 * 
+	 * @param operators The arguments to sum.
+	 * @return The result of the operation.
+	 */
 	BigDecimal add(final List<BigDecimal> operators);
-	
+
+	/**
+	 * Method that returns the difference of the given list, said list must contain two
+	 * operators at least, an {@link IllegalArgumentException} will be thrown
+	 * otherwise.
+	 * 
+	 * @param operators The arguments to subtract.
+	 * @return The result of the operation.
+	 */
 	BigDecimal diff(final List<BigDecimal> operators);
 }
